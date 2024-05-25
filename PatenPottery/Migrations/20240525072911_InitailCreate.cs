d1712c59ab7885409b22a2a0891458a02c7e8422
Darpan <darpan.niraula@gmail.com>
2024-05-25 13:19:37 +0545

    Connecting Database

---
+++ PatenPottery/Migrations/20240525072911_InitailCreate.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PatenPottery.Migrations
+{
+    public partial class InitailCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CustomerDetails",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Date = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerDetails", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomerDetails");
+        }
+    }
+}
---
+++ PatenPottery/Models/PatenPotteryContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PatenPottery.Models
+{
+    public class PatenPotteryContext : DbContext
+    {
+        public DbSet<CustomerDetail> CustomerDetails { get; set; }
+
+        public PatenPotteryContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
