5626366d4ae13a3a5306db75e682a4c73c5502c9
Tohfa Niraula <tohfa.niraula1@gmail.com>
2024-05-25 17:22:25 +0545

    add migration

---
+++ PatenPottery/Migrations/20240525111753_v1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PatenPottery.Migrations
+{
+    public partial class v1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CustomerDetails",
+                table: "CustomerDetails");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "CustomerDetails");
+
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "CustomerDetails",
+                newName: "Number");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CustomerId",
+                table: "CustomerDetails",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "CustomerDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "CustomerDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "CustomerDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CustomerDetails",
+                table: "CustomerDetails",
+                column: "CustomerId");
+
+            migrationBuilder.CreateTable(
+                name: "Codes",
+                columns: table => new
+                {
+                    CodeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ParentCodeId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Codes", x => x.CodeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProcessFlows",
+                columns: table => new
+                {
+                    ProcessFlowID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DoFire = table.Column<bool>(type: "bit", nullable: false),
+                    DoGlaze = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProcessFlows", x => x.ProcessFlowID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderDetails",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerId = table.Column<int>(type: "int", nullable: false),
+                    ProcessFlowID = table.Column<int>(type: "int", nullable: false),
+                    OrderNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Image = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    StatusCD = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDetails", x => x.OrderId);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Codes_StatusCD",
+                        column: x => x.StatusCD,
+                        principalTable: "Codes",
+                        principalColumn: "CodeId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_CustomerDetails_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "CustomerDetails",
+                        principalColumn: "CustomerId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_ProcessFlows_ProcessFlowID",
+                        column: x => x.ProcessFlowID,
+                        principalTable: "ProcessFlows",
+                        principalColumn: "ProcessFlowID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_CustomerId",
+                table: "OrderDetails",
+                column: "CustomerId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_ProcessFlowID",
+                table: "OrderDetails",
+                column: "ProcessFlowID",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_StatusCD",
+                table: "OrderDetails",
+                column: "StatusCD",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderDetails");
+
+            migrationBuilder.DropTable(
+                name: "Codes");
+
+            migrationBuilder.DropTable(
+                name: "ProcessFlows");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CustomerDetails",
+                table: "CustomerDetails");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerId",
+                table: "CustomerDetails");
+
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "CustomerDetails");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "CustomerDetails");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "CustomerDetails");
+
+            migrationBuilder.RenameColumn(
+                name: "Number",
+                table: "CustomerDetails",
+                newName: "Date");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Id",
+                table: "CustomerDetails",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CustomerDetails",
+                table: "CustomerDetails",
+                column: "Id");
+        }
+    }
+}
