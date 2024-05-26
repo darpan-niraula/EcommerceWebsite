7176b12d3434048acdd61d5a22c5cf5863e0c51b
Tohfa Niraula <tohfa.niraula1@gmail.com>
2024-05-26 12:26:33 +0545

    design changes

--- PatenPottery/Controllers/OrderDetailController .cs
+++ PatenPottery/Controllers/OrderDetailController .cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using PatenPottery.Interface;
 using PatenPottery.Models;
 using PatenPottery.ViewModels;
@@ -6,6 +7,7 @@ using System.Diagnostics;
 
 namespace PatenPottery.Controllers
 {
+    [Authorize]
     public class OrderDetailController : Controller
     {
         private readonly ILogger<OrderDetailController> _logger;
